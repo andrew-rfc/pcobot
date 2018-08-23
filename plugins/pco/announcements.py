44cb0ab505e4f0e98bc8a16bcd43f299e62f688c
pastorhudson <3606628+pastorhudson@users.noreply.github.com>
2018-08-23 14:19:24 -0400

    Added !setannouncements command to set the announcement channel.

--- plugins/pco/announcements.py
+++ plugins/pco/announcements.py
@@ -5,16 +5,32 @@ from plugins.pco import birthday, address, phone_numbers, checkins, msg_attachme
 
 class ScheduledAnnounce(WillPlugin):
 
+    def announcement_channel(self, channel=None):
+        """"If you pass a channel it will save it as the announcement channel, and return the announcement channel.
+        If you pass no channel it will return the saved channel. If no channel is set it defaults to the announcements
+        channel."""
+        if not self.load("announcement_channel") and channel is None:
+            self.save("announcement_channel", "announcements")
+        elif channel:
+            self.save("announcement_channel", channel)
+        return self.load("announcement_channel")
+
     # @hear("announce birthdays")
     # @periodic(hour='14', minute='10')  # at a certain time
     @periodic(hour='07', minute='50')  # at a certain time
-    def announce_birthdays(self, channel='announcements'):
-        birthday.announce_todays_birthdays(self, channel=channel)
+    def announce_birthdays(self):
+        birthday.announce_todays_birthdays(self, channel=self.announcement_channel())
+
+    @respond_to("(!setannouncements)(?P<channel>.*?(?=(?:\?)|$))", acl=["admins"])
+    def set_anounce_chan(self, channel):
+        announce_chan = self.announcement_channel(channel=channel)
+        self.reply(self, "Announcement channel is set to %s" % announce_chan)
+
 
     # @periodic(hour='14', minute='10')  # at a certain time
     # @periodic(second=0)  # every minute at 0 seconds
     def announce(self):
-        self.say("Announcement!", channel="general")
+        self.say("Announcement!", channel=self.announcement_channel())
 
     # @hear("this channel")
     def this_channel(self, channel=None):
